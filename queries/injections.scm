35edbf48ecde4061ecde4fdeca0e4085c2c0d659
Wu, Zhenyu <wuzhenyu@ustc.edu>
2026-07-12 01:18:07 +0800

    :art: Inject signature

--- queries/injections.scm
+++ queries/injections.scm
@@ -11,5 +11,20 @@
   (#set! injection.language "expando")
   (#match? @_option "_format$"))
 
+(set_directive
+  (command)
+  (option) @_option
+  content: (string) @injection.content
+  (#set! injection.language "bash")
+  (#match? @_option "_command"))
+
+(set_directive
+  (command)
+  (option) @_option
+  content: (string) @_content @injection.content
+  (#set! injection.language "bash")
+  (#eq? @_option "signature")
+  (#match? @_content "\\|$"))
+
 ((comment) @injection.content
   (#set! injection.language "comment"))
