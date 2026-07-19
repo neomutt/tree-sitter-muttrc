3c5f9c1e60d221f14b04f2a143bd5e5763be5135
Wu, Zhenyu <wuzhenyu@ustc.edu>
2026-07-20 00:49:58 +0800

    :sparkles: Add queries/schema.scm

---
+++ queries/schema.scm
@@ -0,0 +1,15 @@
+(source_directive
+  (path
+    (string) @source.-))
+
+(set_directive
+  (option) @--option
+  (string) @set.-option)
+
+(set_directive
+  (option) @--option
+  (quadoption) @set.-option)
+
+(set_directive
+  (option) @--option
+  (int) @set.-option.--integer)
